7914f30f94a8a6219f0a2c01410e06df3ffea604
xjh_glb <772106613@qq.com>
2022-07-22 23:07:19 +0800

    feat:add the response component

---
+++ internal/pkg/app/pagination.go
@@ -0,0 +1,37 @@
+package app
+
+import (
+	"gin-scaffold/internal/component_name"
+	"gin-scaffold/internal/pkg/convert"
+	"github.com/gin-gonic/gin"
+)
+
+func GetPage(c *gin.Context) int {
+	page := convert.StrTo(c.Query("page")).MustInt()
+	if page <= 0 {
+		return 1
+	}
+
+	return page
+}
+
+func GetPageSize(c *gin.Context) int {
+	pageSize := convert.StrTo(c.Query("page_size")).MustInt()
+	if pageSize <= 0 {
+		return component_name.AppSetting.DefaultPageSize
+	}
+	if pageSize > component_name.AppSetting.MaxPageSize {
+		return component_name.AppSetting.MaxPageSize
+	}
+
+	return pageSize
+}
+
+func GetPageOffset(page, pageSize int) int {
+	result := 0
+	if page > 0 {
+		result = (page - 1) * pageSize
+	}
+
+	return result
+}
